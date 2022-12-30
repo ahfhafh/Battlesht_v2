2a76457001ba0b7865153bf882e17e37274486b9
Jack <ahfhafh@gmail.com>
2022-12-30 00:44:52 -0700

    add shit images and types

---
+++ Battleshit/Images.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Battleshit
+{
+    public static class Images
+    {
+        public readonly static ImageSource Shit_bg = LoadImage("shit_bg.png");
+        public readonly static ImageSource Shit_5 = LoadImage("stool_5.png");
+        public readonly static ImageSource Shit_4 = LoadImage("stool_4.png");
+        public readonly static ImageSource Shit_3 = LoadImage("stool_3.png");
+        public readonly static ImageSource Shit_2 = LoadImage("stool_2.png");
+
+        public readonly static ImageSource Shit_head = LoadImage("stool_head.png");
+        public readonly static ImageSource Shit_body = LoadImage("stool_mid.png");
+        public readonly static ImageSource Shit_tail = LoadImage("stool_tail.png");
+
+        internal static ImageSource image_name;
+
+        private static ImageSource LoadImage(string filename)
+        {
+            return new BitmapImage(new Uri($"Assets/{filename}", UriKind.Relative));
+        }
+    }
+}
--- Battleshit/Shit.cs
+++ Battleshit/Shit.cs
@@ -1,14 +1,23 @@
-﻿namespace Battleshit
+﻿using System.Windows.Media;
+
+namespace Battleshit
 {
     public class Shit
     {
-        private string name;
-        private int length;
+        public string name { get; }
+        public int shit_type { get; }
+        public int length { get; }
+        public readonly static Shit Carrier = new Shit("Carrier", 5, 5);
+        public readonly static Shit Destroyer = new Shit("Destroyer", 1, 2);
+        public readonly static Shit Submarine = new Shit("Submarine", 2, 3);
+        public readonly static Shit Cruiser = new Shit("Cruiser", 3, 3);
+        public readonly static Shit Battleship = new Shit("Battleship", 4, 4);
 
-        public Shit(string Shit_name, int Shit_length)
+        public Shit(string shit_name, int shit_type, int shit_length)
         {
-            name = Shit_name;
-            length = Shit_length;
+            name = shit_name;
+            this.shit_type = shit_type;
+            length = shit_length;
         }
     }
 }
