9f02cb82ffed7260c9116275c63b45ee27fc63e4
Jack <ahfhafh@gmail.com>
2023-01-04 20:35:18 -0700

    Player and computer turn 1/2

--- Battleshit/MainWindow.xaml.cs
+++ Battleshit/MainWindow.xaml.cs
@@ -36,7 +36,7 @@ namespace Battleshit
 
             RotateTransform rotateTransform = new RotateTransform(90);
 
-/*            Print2DArray(boardValues);*/
+            Print2DArray(boardValues);
                 
             for (int x = 0; x < rows; x++)
             {
@@ -44,11 +44,14 @@ namespace Battleshit
                 {
                     Image image = new Image();
 
-                    if (boardValues[x, y] != 0)    // if there is a shit
+                    if (!isPlayerBoard) // allow click on enemy board
                     {
-                        Debug.WriteLine(x + " " + y);
-                        // determine if shit is horizontal or vertical
-                        if ((y != cols - 1) && (y != 0))   // not at left and right edges
+                        image.MouseEnter += new MouseEventHandler(highlightElement);
+                        image.MouseLeave += new MouseEventHandler(unhighlightElement);
+                        image.MouseDown += playerTurnClick;
+                    }
+
+                    switch(boardValues[x, y])
                         {
                             if ((boardValues[x, y] == boardValues[x, y + 1]) && (boardValues[x, y] != boardValues[x, y - 1]))     // if shit is horizontal and head
                             {
@@ -135,28 +138,89 @@ namespace Battleshit
                             }
                         }
                     }
-                    else
-                    {
-                        image.Source = Images.Shit_bg;
-                        images[x, y] = image;
-                        Board.Children.Add(image);
-                    }
-                }
-            }
-   
-            return images;
-        }
 
         public static void Print2DArray<T>(T[,] matrix)
         {
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    System.Diagnostics.Debug.Write(matrix[i, j] + "\t");
+                    {
+                    Debug.Write(matrix[i, j] + "\t");
                 }
-                System.Diagnostics.Debug.WriteLine("|");
+                Debug.WriteLine("|");
+                    }
+                }
+
+        private void highlightElement(object sender, MouseEventArgs e)
+        {
+            Image img = sender as Image;
+            img.Opacity = 0.5;
+            
             }
+   
+        private void unhighlightElement(object sender, MouseEventArgs e)
+        {
+            Image img = sender as Image;
+            img.Opacity = 1;
+        }
+
+        private async void playerTurnClick(object sender, RoutedEventArgs e)
+        {
+            Image img = sender as Image;
+            UniformGrid parent = (UniformGrid)img.Parent;
+
+            // Disable click
+
+
+            int index = parent.Children.IndexOf(img);
+            int x = index / 10;
+            int y = index % 10;
+
+            // check if shit tried already
+            if ((gamestate.Board2[x, y] == BoardValues.Destroyed) || (gamestate.Board2[x, y] == BoardValues.Miss))
+            {
+                return;
+            }
+            // Check if is shit
+            if (img.Source != Images.Shit_bg)
+        {
+                // Mark as clicked
+                gamestate.Board2[x, y] = BoardValues.Destroyed;
+                DrawBoard(boardImages2, gamestate.Board2);
+            } else
+            {
+                // Mark as clicked
+                gamestate.Board2[x, y] = BoardValues.Miss;
+                DrawBoard(boardImages2, gamestate.Board2);
+            }
+
+            // Do computer turn
+            GameStatusLabel.Content = "Wait for computer...";
+            await Task.Delay(1000);
+            int rnd_x = random.Next(0, cols);
+            int rnd_y = random.Next(0, rows);
+
+            // check if shit tried already
+            while ((gamestate.Board1[rnd_x, rnd_y] == BoardValues.Destroyed) || (gamestate.Board1[rnd_x, rnd_y] == BoardValues.Miss))
+                {
+                rnd_x = random.Next(0, cols);
+                rnd_y = random.Next(0, rows);
+                }
+            if (gamestate.Board1[rnd_x, rnd_y] != BoardValues.Empty) 
+            {
+                // Mark as clicked
+                gamestate.Board1[rnd_x, rnd_y] = BoardValues.Destroyed;
+                DrawBoard(boardImages1, gamestate.Board1);
+            } else
+            {
+                // Mark as clicked
+                gamestate.Board1[rnd_x, rnd_y] = BoardValues.Miss;
+                DrawBoard(boardImages1, gamestate.Board1);
+            }
+
+            // Enable Click
+            GameStatusLabel.Content = "Your turn";
+
         }
 
         /*private void Shit_MLButtonDown(object sender, MouseButtonEventArgs e)
